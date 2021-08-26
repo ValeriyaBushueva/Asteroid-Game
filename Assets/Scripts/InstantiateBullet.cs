949011c4f427205fecba2fa8578510dc0d4aa2ac
Valeriya Bushueva <valeriya_sityaeva1995@mail.ru>
2021-08-26 13:11:53 +0300

    create pool object

--- Assets/Scripts/InstantiateBullet.cs
+++ Assets/Scripts/InstantiateBullet.cs
@@ -1,14 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class InstantiateBullet: MonoBehaviour
     {
         [SerializeField] private Rigidbody2D bullet;
         [SerializeField] private Transform barrel;
         [SerializeField] private float force;
-        
+
+        private ObjectPooler objectPooler;
+
+        private void Start()
+        {
+            objectPooler =ObjectPooler.Instance;
+        }
+
         public void BulletSpawn()
         {
-            var temAmmunition = Instantiate(bullet, barrel.position, barrel.rotation);
-            temAmmunition.AddForce(barrel.up * force);
+            ObjectPooler.Instance.SpawnFromPool("Laser", transform.position, Quaternion.identity);
+            // var temAmmunition = Instantiate(bullet, barrel.position, barrel.rotation);
+            // temAmmunition.AddForce(barrel.up * force);
         }
     }
