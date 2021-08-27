7d107e95f076c8e86d75554095316d7c63ad26bd
Valeriya Bushueva <valeriya_sityaeva1995@mail.ru>
2021-08-27 19:10:31 +0300

    bullet destroy enemy

---
+++ Assets/Scripts/BulletCollision.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+    public class BulletCollision: MonoBehaviour
+
+    {
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
